0a1f93a64e059f56cd07926704dc009363f46e1f
iLLiSiO2 <samsudar200@gmail.com>
2025-01-15 09:21:10 +0530

    Ready for deployment

--- EyeCare/vite.config.js
+++ EyeCare/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output directory for production build
+    assetsDir: "assets", // Directory for static assets
+  },
+});
