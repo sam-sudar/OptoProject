12276e7a0cc80008c66a8a81bb238621eec98d03
iLLiSiO2 <samsudar200@gmail.com>
2025-01-15 20:00:51 +0530

    Image issue

--- EyeCare/data.js
+++ EyeCare/data.js
@@ -5,7 +5,7 @@ export const blogData = [
     author: "Abisha S",
     link: "https://visionscienceacademy.org/the-crucial-role-of-optical-store-experience-in-an-optometrists-career/",
     title: "The Optical Store's Role in Optometry",
-    image: "/src/assets/Blog_OpticsStore1.jpg",
+    image: "/assets/Blog_OpticsStore1.jpg",
     content:
       "Being an optometrist involves more than just giving a prescription and dispensing eyeglasses. This marks the beginning of a crucial process for both the dispenser and the customer. In this article, we will explore the significance of working in an optical store and how this experience contributes to an optometrist's career.",
   },
@@ -14,7 +14,7 @@ export const blogData = [
     author: "Abisha S",
     link: "https://visionscienceacademy.org/covid-19s-mark-on-the-eyes/",
     title: "Covid's Mark on the Eyes",
-    image: "/src/assets/Blog_Covid.jpg",
+    image: "/assets/Blog_Covid.jpg",
     content:
       "This blog discusses common eye progsjagsjggggggggggggggggggggggggggggggggggggggggggggggggggg",
   },
@@ -23,7 +23,7 @@ export const blogData = [
     author: "Abisha S",
     link: "https://visionscienceacademy.org/dont-lose-the-permanent-treasure-for-the-temporary-pleasure/",
     title: "Value the Permanent Over the Temporary",
-    image: "/src/assets/Blog_EyeCare1.jpg",
+    image: "/assets/Blog_EyeCare1.jpg",
     content:
       "Being an optometrist involves more than just giving a prescription and dispensing eyeglasses. This marks the beginning of a crucial process for both the dispenser and the customer. In this article, we will explore the significance of working in an optical store and how this experience contributes to an optometrist's career.",
   },
