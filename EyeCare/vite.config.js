c7ef527b0458fdf3a32ebe41082872c645f4aa55
iLLiSiO2 <samsudar200@gmail.com>
2025-01-15 09:49:52 +0530

    Post deployment

--- EyeCare/vite.config.js
+++ EyeCare/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import VitePluginSitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: "https://eye-and-my-care-optics.vercel.app", // Your website URL
+      routes: [
+        { url: "/", changefreq: "daily", priority: 1.0 },
+        { url: "/about", changefreq: "monthly", priority: 0.8 },
+        { url: "/services", changefreq: "weekly", priority: 0.9 },
+        { url: "/blogs", changefreq: "weekly", priority: 0.7 },
+        { url: "/contacts", changefreq: "yearly", priority: 0.6 },
+      ],
+    }),
+  ],
   build: {
-    outDir: "dist", // Output directory for production build
-    assetsDir: "assets", // Directory for static assets
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
